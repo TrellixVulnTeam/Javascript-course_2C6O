ccb5a08b02205a0648132a622000bc19530c7f8f
Craig <CraigKapan@gmail.com>
2021-12-02 18:15:25 +0200

    Coding Challenge #2

--- complete-javascript-course-master/complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/14-OOP/starter/script.js
@@ -211,7 +211,7 @@ const personProto = {
   init(firstName, birthYear) {
     this.firstName = firstName;
     this.birthYear = birthYear;
-  }
+  },
 };
 
 const steven = Object.create(personProto);
@@ -225,3 +225,39 @@ console.log(steven.__proto__ === personProto);
 const sarah = Object.create(personProto);
 sarah.init("Sarah", 1979);
 sarah.calcAge();
+
+/////////////////////////////////////////
+// Coding Challenge #2
+
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  accelerate = function () {
+    this.speed += 10;
+    console.log(`${this.make} is going at ${this.speed}km/h`);
+  };
+
+  brake = function () {
+    this.speed -= 5;
+    console.log(`${this.make} is going at ${this.speed}km/h`);
+  };
+
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+
+  set speedUS(speed) {
+    this.speed * speed * 1.6
+  }
+}
+
+const ford = new CarCl("Ford", 120);
+console.log(ford.speedUS)
+ford.accelerate();
+ford.accelerate();
+ford.brake();
+ford.speedUS = 50;
+console.log(ford)
