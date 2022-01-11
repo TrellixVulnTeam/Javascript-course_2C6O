73ab492ad9e3def51abeb40469df8e754d9d6511
Craig <CraigKapan@gmail.com>
2022-01-11 10:59:37 +0200

    Project Architecture

--- complete-javascript-course-master/complete-javascript-course-master/15-Mapty/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/15-Mapty/starter/script.js
@@ -1,26 +1,26 @@
-"use strict"; // Javascript-course\complete-javascript-course-master\complete-javascript-course-master\15-Mapty
+"use strict"; // Javascript-course\complete-javascript-course-master\complete-javascript-course-master\15-Mapty 
 
 // prettier-ignore
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']; 
 
 const form = document.querySelector(".form");
 const containerWorkouts = document.querySelector(".workouts");
 const inputType = document.querySelector(".form__input--type");
-const inputDistance = document.querySelector(".form__input--distance");
-const inputDuration = document.querySelector(".form__input--duration");
+const inputDistance = document.querySelector(".form__input--distance"); 
+const inputDuration = document.querySelector(".form__input--duration"); 
 const inputCadence = document.querySelector(".form__input--cadence");
-const inputElevation = document.querySelector(".form__input--elevation");
+const inputElevation = document.querySelector(".form__input--elevation"); 
 
 let map, mapEvent;
 
 if (navigator.geolocation)
   navigator.geolocation.getCurrentPosition(
-    function (position) {
+    function (position) { 
       const { latitude } = position.coords;
       const { longitude } = position.coords;
       console.log(`https://www.google.co.za/maps/@${latitude},${longitude}`);
-
-      const coords = [latitude, longitude];
+ 
+      const coords = [latitude, longitude]; 
 
       map = L.map("map").setView(coords, 13);
       // console.log(map);
