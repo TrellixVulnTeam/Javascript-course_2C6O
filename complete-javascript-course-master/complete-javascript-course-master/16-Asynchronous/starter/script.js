2b8c43e0ed2847136fde93782404485fb45978ba
Craig <CraigKapan@gmail.com>
2022-03-12 11:36:29 +0200

    Error Handling With try...catch

--- complete-javascript-course-master/complete-javascript-course-master/16-Asynchronous/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/16-Asynchronous/starter/script.js
@@ -392,7 +392,6 @@ createImage("img/img-1.jpg")
   .catch(err => console.error(err));
 */
 
-
 const getPosition = function () {
   return new Promise(function (resolve, reject) {
     navigator.geolocation.getCurrentPosition(resolve, reject);
@@ -402,12 +401,14 @@ const getPosition = function () {
 // fetch(`https://restcountries.com/v2/name/${country}`).then(res => console.log(res))
 
 const whereAmI = async function () {
-  // Geolocation
+  try {
+    // Geolocation
   const pos = await getPosition();
   const { latitude: lat, longitude: lng } = pos.coords;
 
   // Reverse geocoding
   const resGeo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
+  if(!resGeo.ok) throw new Error("Problem getting location data")
   const dataGeo = await resGeo.json();
   console.log(dataGeo);
 
@@ -415,9 +416,23 @@ const whereAmI = async function () {
   const res = await fetch(
     `https://restcountries.com/v2/name/${dataGeo.country}`
   );
+  if(!res.ok) throw new Error("Problem getting country")
+
   const data = await res.json();
   console.log(data);
   renderCountry(data[0]);
+  } catch(err) {
+    console.error(`${err} 💥`);
+    renderErr(`💥 ${err.message}`)
+  }
 };
 whereAmI();
 console.log("FIRST");
+
+// try {
+//   let y = 1;
+//   const x = 2;
+//   y = 3;
+// } catch(err) {
+//   alert(err.message)
+// }
