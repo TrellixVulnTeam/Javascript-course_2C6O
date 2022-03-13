38f602065a7e9e77a64847446dfdc4aecee9c4c1
Craig <CraigKapan@gmail.com>
2022-03-13 12:51:43 +0200

    Returning Values from Async Functions

--- complete-javascript-course-master/complete-javascript-course-master/16-Asynchronous/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/16-Asynchronous/starter/script.js
@@ -403,36 +403,46 @@ const getPosition = function () {
 const whereAmI = async function () {
   try {
     // Geolocation
-  const pos = await getPosition();
-  const { latitude: lat, longitude: lng } = pos.coords;
+    const pos = await getPosition();
+    const { latitude: lat, longitude: lng } = pos.coords;
 
-  // Reverse geocoding
-  const resGeo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
-  if(!resGeo.ok) throw new Error("Problem getting location data")
-  const dataGeo = await resGeo.json();
-  console.log(dataGeo);
+    // Reverse geocoding
+    const resGeo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
+    if (!resGeo.ok) throw new Error("Problem getting location data");
+    const dataGeo = await resGeo.json();
+    // Country data
+    const res = await fetch(
+      `https://restcountries.com/v2/name/${dataGeo.country}`
+    );
+    if (!res.ok) throw new Error("Problem getting country");
+    const data = await res.json();
+    renderCountry(data[0]);
 
-  // Country data
-  const res = await fetch(
-    `https://restcountries.com/v2/name/${dataGeo.country}`
-  );
-  if(!res.ok) throw new Error("Problem getting country")
-
-  const data = await res.json();
-  console.log(data);
-  renderCountry(data[0]);
-  } catch(err) {
+    return `You are in ${dataGeo.city}, ${dataGeo.country}`;
+  } catch (err) {
     console.error(`${err} 💥`);
-    renderErr(`💥 ${err.message}`)
+    renderErr(`💥 ${err.message}`);
+
+    // Reject promise returned from async function
+    throw err;
   }
 };
-whereAmI();
-console.log("FIRST");
 
-// try {
-//   let y = 1;
-//   const x = 2;
-//   y = 3;
-// } catch(err) {
-//   alert(err.message)
-// }
+console.log("1: Will get location");
+// const city = whereAmI();
+// console.log(city);
+
+// whereAmI()
+//   .then(city => console.log(`2: ${city}`))
+//   .catch(err => console.error(`2: ${err.message} 💥`))
+//   .finally(() => console.log("3: Finished getting location"));
+
+(async function () {
+  try {
+    const city = await whereAmI();
+    console.log(`2: ${city}`);
+  } catch (err) {
+    console.error(`2: ${err.message} 💥`);
+  }
+  console.log("3: Finished getting location");
+})();
