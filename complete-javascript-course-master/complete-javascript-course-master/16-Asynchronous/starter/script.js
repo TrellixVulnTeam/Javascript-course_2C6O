d4ed4489832cc0cb766a6d1ac28bff1a38b69c77
Craig <CraigKapan@gmail.com>
2022-03-19 09:25:19 +0200

    Coding Challenge #3

--- complete-javascript-course-master/complete-javascript-course-master/16-Asynchronous/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/16-Asynchronous/starter/script.js
@@ -478,6 +478,7 @@ const get3Countries = async function (c1, c2, c3) {
 get3Countries("portugal", "canada", "tanzania");
 */
 
+/*
 // Promise.race
 (async function () {
   const res = await Promise.race([
@@ -527,3 +528,38 @@ Promise.any([
 ])
   .then(res => console.log(res))
   .catch(err => console.error(err));
+*/
+
+/*
+const loadNPause = async function () {
+  try {
+    // Load image 1
+    let img = await createImage('img/img-1.jpg');
+    console.log('Image 1 loaded');
+    await wait(2);
+    img.style.display = 'none';
+
+    // Load image 1
+    img = await createImage('img/img-2.jpg');
+    console.log('Image 2 loaded');
+    await wait(2);
+    img.style.display = 'none';
+  } catch (err) {
+    console.error(err);
+  }
+};
+// loadNPause();
+
+// PART 2
+const loadAll = async function (imgArr) {
+  try {
+    const imgs = imgArr.map(async img => await createImage(img));
+    const imgsEl = await Promise.all(imgs);
+    console.log(imgsEl);
+    imgsEl.forEach(img => img.classList.add('parallel'));
+  } catch (err) {
+    console.error(err);
+  }
+};
+loadAll(['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg']);
+*/
