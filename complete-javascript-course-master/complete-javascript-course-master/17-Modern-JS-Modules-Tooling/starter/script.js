994fd8ed74dab33fda0e05400ec91083579d015b
Craig <CraigKapan@gmail.com>
2022-03-20 11:12:20 +0200

    Top-Level await (ES2022)

--- complete-javascript-course-master/complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,5 +1,8 @@
 //Javascript-course\complete-javascript-course-master\complete-javascript-course-master\17-Modern-JS-Modules-Tooling
 
+///////////////////////////////////////
+// Exporting and Importing in ES6 Modules
+
 // Importing module
 // import {addToCart, totalPrice as price, tq } from "./shoppingCart.js";
 // addToCart("bread", 5);
@@ -16,8 +19,33 @@ console.log("Importing module");
 // console.log(price);
 
 import add, { cart } from "./shoppingCart.js";
-add("pizza", 2)
-add("bread", 5)
-add("apples", 4)
+add("pizza", 2);
+add("bread", 5);
+add("apples", 4);
 
 console.log(cart);
+
+///////////////////////////////////////
+// Top-Level await (ES2022)
+
+// console.log("Start fetching");
+// const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+// const data = await res.json();
+// console.log(data);
+// console.log("Something");
+
+const getLastPost = async function () {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await res.json();
+
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+const lastPost = getLastPost();
+console.log(lastPost);
+
+// Not very clean
+// lastPost.then(last => console.log(last))
+
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
