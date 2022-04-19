34ad03d47d22dcd5203366b2e18d2554899afb3f
Craig <CraigKapan@gmail.com>
2022-04-19 19:14:21 +0200

    Configuring Babel and Polyfilling

--- complete-javascript-course-master/complete-javascript-course-master/17-Modern-JS-Modules-Tooling/src/index.js
+++ complete-javascript-course-master/complete-javascript-course-master/17-Modern-JS-Modules-Tooling/src/index.js
@@ -3,7 +3,6 @@
 ///////////////////////////////////////
 // Exporting and Importing in ES6 Modules
 
-
 // Importing module
 // import {addToCart, totalPrice as price, tq } from "./shoppingCart.js";
 // addToCart("bread", 5);
@@ -26,7 +25,6 @@ add("bread", 5);
 add("apples", 4);
 
 console.log(cart);
-
 
 /*
 ///////////////////////////////////////
@@ -104,7 +102,6 @@ export.addTocart = function (product, quantity) {
 const { addTocart } = require('./shoppingCart');
 */
 
-
 ///////////////////////////////////////
 // Introduction to NPM
 
@@ -128,3 +125,24 @@ console.log(stateDeepClone);
 if (module.hot) {
   module.hot.accept();
 }
+
+class Person {
+  #greeting = "Hey";
+  constructor(name) {
+    this.name = name;
+    console.log(`${this.#greeting}, ${this.name}`);
+  }
+}
+const jonas = new Person("Jonas");
+
+console.log("Jonas" ?? null);
+
+console.log(cart.find(el => el.quantity >= 2));
+Promise.resolve("TEST").then(x => console.log(x))
+
+import 'core-js/stable';
+// import "core-js/stable/array/find";
+// import "core-js/stable/promise";
+
+// Polifilling async functions
+import "regenerator-runtime/runtime";
--- complete-javascript-course-master/complete-javascript-course-master/17-Modern-JS-Modules-Tooling/webpack.config.js
+++ complete-javascript-course-master/complete-javascript-course-master/17-Modern-JS-Modules-Tooling/webpack.config.js
@@ -1,25 +1,49 @@
-const HtmlWebPackPlugin = require('html-webpack-plugin');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+require("babel-register");
 
 module.exports = {
-    mode: 'none',
-    module: {
-        rules: [
-            {
-                test: /\.html$/,
-                use: [
-                    {
-                        loader: "html-loader",
-                        options: { minimize: true }
-                    }
-                ]
-                
-            },
-        ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/index.html",
-            filename: "./index.html"
-        }),
-    ]
-}
+  mode: "none",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true },
+          },
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+    }),
+  ],
+};
